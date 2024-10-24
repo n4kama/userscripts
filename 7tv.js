1909f5f9a54a7d3e7584affada065ba5ccc7ac33
nakama <nakama@baguet.org>
2024-10-24 09:58:08 +0000

    7tv userscript header updated

--- 7tv.js
+++ 7tv.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         7tv .webp to .png
-// @namespace    https://git.baguet.org/nakama/userscripts/7tv.js
+// @name         7tv replace webp to png
+// @namespace    https://greasyfork.org/en/users/1385483-nakama
 // @version      2024-10-24
-// @description  Changes the raw .wepp url to .png on 7tv.app
-// @author       You
+// @description  Replace the .webp extension to .png in the URL.
+// @author       nakama
 // @match        https://cdn.7tv.app/emote/*/*.webp
 // @run-at       document-start
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=7tv.app
